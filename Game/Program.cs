4471421e44bfc12c37657f9afa2f4dca0a590cb6
Haohan Zhang <59599226+hhlovesyy@users.noreply.github.com>
2025-02-08 18:41:58 +0800

    Update Program.cs

--- Game/Program.cs
+++ Game/Program.cs
@@ -7,7 +7,7 @@ namespace Game
         public static void Main(string[] args)
         {
             //开始的时候服务端连接数据库,
-            if (!DbManager.Connect("game", "127.0.0.1", 3306, "root", "jdjjc552422"))
+            if (!DbManager.Connect("game", "127.0.0.1", 3306, "root", "Admin"))
             {
                 Console.WriteLine("Can't connect with DB");
                 return;
